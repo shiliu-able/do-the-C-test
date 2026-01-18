3cecaf83d15cf1b0a825c804c88e559482b82041
AdministratorPC <cquptlrw@126.com>
2026-01-18 21:03:08 +0800

    二次修改提交

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 // TIP 要<b>Run</b>代码，请按 <shortcut actionId="Run"/> 或点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 int main() {
     // TIP 当文本光标位于 <b>lang</b> 变量名称处时，按 <shortcut actionId="RenameElement"/> 可以查看 CLion 如何帮助您重命名该变量。
-    auto lang1 = "C++";
-    std::cout << "Hello and welcome to " << lang1 << "!\n";
+    auto lang2 = "C++";
+    std::cout << "Hello and welcome to " << lang2 << "!\n";
 
     for (int i = 1; i <= 5; i++) {
         // TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已为您设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点，但您可以随时按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
